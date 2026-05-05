8a80a961bb6ded535a9ceadde6e32e1808ab4f98
sadaquat2305 <sadaquat2305@gmail.com>
2026-05-05 17:42:19 +0530

    Initial commit

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -1,15 +1,61 @@
-//
-//  ViewController.swift
-//  EggTimer
-//
-//  Created by Angela Yu on 08/07/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    @IBOutlet weak var labelText: UILabel!
+    
+    let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 7]
+    
+    var secondsRemaining = 0
+    var totalTime = 0   // ✅ total time store
+    var timer: Timer?
 
-
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        // Stop previous timer
+        timer?.invalidate()
+        
+        guard let hardness = sender.currentTitle,
+              let time = eggTimes[hardness] else {
+            return
+        }
+        
+        labelText.text = hardness
+        
+        totalTime = time              // ✅ set total time
+        secondsRemaining = time
+        
+        progressBar.progress = 0.0    // ✅ reset progress
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0,
+                                     target: self,
+                                     selector: #selector(updateCountdown),
+                                     userInfo: nil,
+                                     repeats: true)
+    }
+    
+    @objc func updateCountdown() {
+        
+        if secondsRemaining > 0 {
+            
+            secondsRemaining -= 1
+            
+            // ✅ Calculate progress
+            let progress = Float(totalTime - secondsRemaining) / Float(totalTime)
+            progressBar.progress = progress
+            
+            print("\(secondsRemaining) seconds")
+            
+        } else {
+            timer?.invalidate()
+            timer = nil
+            progressBar.progress = 1.0  // ✅ full
+            
+            labelText.text = "Done"
+            
+            print("Done!")
+        }
+    }
 }
